54080999fc728a999f3fc5f8e3389657c423b796
moussa <cifadel1@hotmail.com>
2016-10-22 14:42:52 -0400

    creation de nouveau methode

--- telApp/lib/telApp/telApp.rb
+++ telApp/lib/telApp/telApp.rb
@@ -1,48 +1,29 @@
 module TelApp
-  
-  def self.supprimer(nom, prenom, lignes )
-    lignes
-    .reject { |ligne| /#{nom} and #{prenom}/ =~ ligne }
-  end
-  
-  
-  def self.afficher( nom, prenom, lignes )
-    res = []
-    lignes.each do |ligne|
-    res << ligne if /#{nom} and #{prenom}/ =~ ligne
-      res << ligne
-  end
-    res
-  end
 
+  class Contact
 
-  def self.modifier( nomRech, prenomRech, numeroRech, codePostalRech, nom, prenom, numero, codePostal, global, lignes )
-    lignes.map do |ligne|
-      if /#{nom} and #{prenom}/ =~ ligne
-        ligne
-            .send (global ? :gsub : :sub), /#{nomRecher}/, nom, /#{prenomRech}/, numero, /#{numeroRech}/, /#{codePostalRech}/, codePostal
-            else
-        ligne
-      end
+    attr_accessor :nom, :prenom,:numeroTel
+
+    def initialize
+      @nom=@prenom=@numeroTel=""
     end
-  end
 
-  def self.rechercher( nom, prenom, lignes )
-    lignes.map do |ligne|
-      if /#{nom} and #{prenom}/ =~ ligne
-        ligne.print(ligne)
-      else
-        ligne
-      end
+    def to_s
+      "" + @nom + "," + @prenom + "," + @numeroTel + "\n"
     end
-  end
 
-  def self.ajouter( nom, prenom, numero, codePostal, lignes )
-  res = []
-  lignes.each do |ligne|
-    res << (nom + prenom + numero + codePostal + "\n") if /#{nom} and #{prenom}/ =~ ligne
-    res << ligne
   end
-  res
+  def self.afficher
+    contact = Contact.new
+    contact.nom="Moussa"
+    contact.prenom="Balla"
+    contact.numeroTel="514374758"
+    puts contact
+
+  end
+  def self.montest
+
+    puts 'Mon test a marche'
+
   end
 end
---
+++ telApp/lib/telApp/test.rb
@@ -0,0 +1,23 @@
+
+
+class Contact
+
+  attr_accessor :nom, :prenom,:numeroTel
+
+  def initialize
+    @nom=@prenom=@numeroTel=""
+  end
+
+  def to_s
+    "    " + @nom + "," + @prenom + ", " + @numeroTel + "\n"
+  end
+
+end
+def self.aff
+  contact = Contact.new
+  contact.nom="Moussa"
+  contact.prenom="Balla"
+  contact.numeroTel="514374758"
+  puts.contact
+
+end
