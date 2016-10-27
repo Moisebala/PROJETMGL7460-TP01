132e23c0e65a13b01abc37448a4a8cabff8a1f57
moussa <cifadel1@hotmail.com>
2016-10-27 19:45:07 -0400

    module test implementer

--- telApp/lib/telApp/telApp.rb
+++ telApp/lib/telApp/telApp.rb
@@ -1,5 +1,5 @@
 module TelApp
-require 'test'
+
   class Contact
 
     attr_accessor :nom, :prenom,:numeroTel
@@ -32,12 +32,12 @@ require 'test'
     end
     end
   
-  def self.supprimer(nom)
-    
-      File.open("contacts.txt","w+") do |f|
+  def self.supprimer(nom,prenom,nom_fichier)
+
+      File.open(nom_fichier,"w+") do |f|
    f.each do |line|
      
-    if line.match( /#{nom}/i )
+    if line.match( /#{nom} and #{prenom}/i )
      line.reject
      
     end
@@ -45,37 +45,34 @@ require 'test'
     end
   end
 
-  def self.afficher
+  def self.afficher(nom_fichier)
 
-   bd=File.open("contacts.txt","a+")
-   bd.readlines("contacts.txt").each do |line|
+   bd=File.open(nom_fichier,"a+")
+   bd.readlines(nom_fichier).each do |line|
    puts line
    bd.close
 
    end
   end
 
-  def self.ajouter
-    bd=File.open("contacts.txt","a+")
+  def self.ajouter(nom,prenom,numeroTel,nom_fichier)
+    bd=File.open(nom_fichier,"a+")
     contact= Contact.new
-    puts 'nom :'
-    contact.nom=gets.chomp
-    puts 'prenom :'
-    contact.prenom=gets.chomp
-    puts 'numero:'
-    contact.numeroTel=gets.chomp
+    contact.nom=nom
+    contact.prenom=prenom
+    contact.numeroTel=numeroTel
     puts "Le contact a ete ajouter avec succes :"
     puts  contact
     bd.puts(contact)
     bd.close
   end
 
-  def self.rechercher(nom)
-     File.open("contacts.txt","r+") do |f|
+  def self.rechercher(nom,nom_fichier)
+     File.open(nom_fichier,"r+") do |f|
    f.each do |line|
    
     if line.match( /#{nom}/i )
-     print line
+     puts line
     end
     end
     end
