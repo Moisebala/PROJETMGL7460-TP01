b2b85894667f13eb9c91d053de65f9b6f524565a
moussa <cifadel1@hotmail.com>
2016-11-02 15:27:38 -0400

    separation de module et ajout de methode contact_trouver

--- telApp/lib/telApp/fichier_text.rb
+++ telApp/lib/telApp/fichier_text.rb
@@ -1,6 +1,6 @@
 module Fichier
 
-def  self.ouvre_fichier(nom_fichier,repertoire)
+def  self.ouvrir_fichier(nom_fichier,repertoire)
   if File.exist?(nom_fichier)
   bd = File.open(nom_fichier,"a+")
   bd.each_line do |line|
@@ -18,6 +18,25 @@ end
   end
   end
 
+def self.copieTableau(contact, nom,nom_fichier)
+  rep = []
+  rep2 =[]
+   ouvrir_fichier(nom_fichier, rep)
+   rep.each do |line| unless line.match(/#{nom}/i)
+                       rep2 << line
+                     end
+     end
+  rep2.push contact
+  ecrire_fichier(nom_fichier, rep2)
+end
+
+def self.trouver_contact(repertoire,nom)
+  repertoire.select do |line|
+    if line.match( /#{nom}/i )
+      return line
+    end
+  end
+end
 
 end
 
--- telApp/lib/telApp/telApp.rb
+++ telApp/lib/telApp/telApp.rb
@@ -3,7 +3,7 @@ module TelApp
   def self.modifier(nom,numero,nom_fichier)
     contact = Contact.new
     rep = []
-    Fichier.ouvre_fichier(nom_fichier, rep)
+    Fichier.ouvrir_fichier(nom_fichier, rep)
     rep.each do |line| value = line.split(',')
       value.each  do |name|
       if name.match( /#{nom}/i )
@@ -14,67 +14,47 @@ module TelApp
       end
       end
     end
-    copieTableau(contact, nom,nom_fichier)
+    Fichier.copieTableau(contact, nom,nom_fichier)
     return contact
   end
 
 
-  def self.copieTableau(contact, nom,nom_fichier)
+  def self.supprimer(nom,nom_fichier)
     rep = []
-    rep2 =[]
-    Fichier.ouvre_fichier(nom_fichier, rep)
+    rep2=[]
+    Fichier.ouvrir_fichier(nom_fichier, rep)
     rep.each do |line| unless line.match(/#{nom}/i)
                          rep2 << line
                        end
     end
-    rep2.push contact
     Fichier.ecrire_fichier(nom_fichier, rep2)
-  end
-
-  def self.supprimer(nom,nom_fichier)
-    rep = []
-    rep2=[]
-    Fichier.ouvre_fichier(nom_fichier, rep)
-    rep.each do |line| unless line.match(/#{nom}/i)
-                  rep2 << line
-                       end
-    end
-    Fichier.ecrire_fichier(nom_fichier, rep2)
-    rep.each do |line|
-      if line.match( /#{nom}/i )
-        return line
-      end
-    end
+    Fichier.trouver_contact(rep , nom)
   end
 
 
   def self.afficher(nom_fichier)
    rep = []
-   Fichier.ouvre_fichier(nom_fichier, rep)
-    return rep
+   Fichier.ouvrir_fichier(nom_fichier, rep)
+   return rep
   end
 
 
   def self.ajouter(nom,prenom,numeroTel,nom_fichier)
     rep = []
-    Fichier.ouvre_fichier(nom_fichier , rep)
+    Fichier.ouvrir_fichier(nom_fichier , rep)
     contact = Contact.new
     contact.nom = nom
     contact.prenom = prenom
     contact.numeroTel = numeroTel
     rep.push contact
     Fichier.ecrire_fichier(nom_fichier, rep)
-    return rep
+    return contact
   end
+
 
   def self.rechercher(nom,nom_fichier)
     rep = []
-    Fichier.ouvre_fichier(nom_fichier, rep)
-     rep.each  do |line|
-     if line.match( /#{nom}/i )
-       return line
-
-     end
-    end
+    Fichier.ouvrir_fichier(nom_fichier, rep)
+    Fichier.trouver_contact(rep , nom)
   end
 end
